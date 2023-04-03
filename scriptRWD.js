85eed1db45719fed444d5f267f6d4a17a807096d
PutraIsHere <yonorondap@gmail.com>
2023-04-03 19:20:43 +0700

    first commit

---
+++ scriptRWD.js
@@ -0,0 +1,26 @@
+//Get the button 
+var mybutton = document.getElementById("myBtn"); 
+ 
+// When the user scrolls down 20px from the top of the document, show the button 
+window.onscroll = function() {scrollFunction()}; 
+ 
+ 
+function scrollFunction() {   if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {     mybutton.style.display = "block"; 
+  } else {     mybutton.style.display = "none"; 
+  } 
+} 
+ 
+// When the user clicks on the button, scroll to the top of the document 
+function topFunction() {  
+ document.body.scrollTop = 0;  
+  document.documentElement.scrollTop = 0; 
+} 
+ 
+function myMenu() {   
+var x = document.getElementById("myTopnav");   
+if (x.className === "topnav") {     
+x.className += " responsive"; 
+  } else { 
+    x.className = "topnav"; 
+  } 
+} 
